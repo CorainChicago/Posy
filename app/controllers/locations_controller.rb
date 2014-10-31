326af9400c4e21492e216904dca5e19ca1f482fd
Wil Chandler <wilchandler2@gmail.com>
2014-10-31 16:27:53 -0500

    Started sidebar HTML/CSS

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,6 +3,7 @@ class LocationsController < ApplicationController
   def show
     url = params[:slug].downcase.strip
     @location = Location.find_by(slug: url)
+    @post = Post.new
     redirect_to '/' unless @location
   end
 
