2f3324b24da4ae9aa35baa5311a5ebf3a48090e8
Wil Chandler <wilchandler2@gmail.com>
2014-11-03 18:04:11 -0600

    Reactified sidebar

--- app/assets/javascripts/location.js.jsx
+++ app/assets/javascripts/location.js.jsx
@@ -1,19 +1,115 @@
 /** @jsx React.DOM */
-var postsPath = window.location.pathname + "/posts";
 
-var Post = React.createClass({
+var LocationDisplay = React.createClass({
+  getInitialState: function() {
+    return {posts: []};
+  },
+  componentDidMount: function() {
+    this.loadPostsFromServer(this.props.batchSize);
+    setInterval(this.loadPostsFromServer, this.props.pollInterval);
+    $(window).on('scroll', this.handleScroll);
+    $('form[id=post-form]').on('submit', this.handlePostSubmit);
+  },
+  loadPostsFromServer: function(size) {
+    var size = size || this.state.posts.length
+
+    $.ajax({
+      url: this.props.url,
+      data: { batch_size: size },
+      dataType: 'json',
+      success: function (data) {
+        this.setState({posts: data.posts});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  handleScroll: function() {
+    if ( $(window).scrollTop() == $(document).height() - $(window).height()) {
+      newSize = this.state.posts.length + this.props.batchSize;
+      this.loadPostsFromServer(newSize);
+    };
+  },
+  handlePostSubmit: function(event) {
+    event.preventDefault();
+    var location = this;
+    var $form = $(event.target);
+
+    console.log(this.props.url);
+    $.ajax({
+      type: "POST",
+      url: this.props.url,
+      data: $form.serializeArray()
+    })
+    .done(function(response) {
+      newPost = response.post;
+      location.setState({posts: [newPost].concat(location.state.posts)});
+    })
+    .fail(function(response) {
+      var errors = response.responseJSON.errors
+    })
+  },
   render: function() {
     return (
-      <div className="post">
-        <p className="location">{this.props.location}: </p>
-        <p className="hair">{this.props.hair}-haired </p>
-        <p className="gender">{this.props.gender}</p>
-        <p className="content">{this.props.content}</p>
-        <a className="post-flag" href="#" title="Flag as inappropriate">X</a>
+      <div id="location-container">
+        <Sidebar />
+        <div id="location_posts">
+            <PostList posts={this.state.posts} />
+        </div>
       </div>
-    );
+    )
   }
 });
+
+var Sidebar = React.createClass({
+  render: function() {
+    return(
+      <div id="sidebar">
+        <div id="sidebar-container">
+          <h1 id="badge">afar</h1>
+          <h2 id="location">{locationName}</h2>
+          <SidebarPostForm />
+        </div>
+      </div>
+    )
+  }
+});
+
+var SidebarPostForm = React.createClass({
+  render: function() {
+    return (
+      <form accept-charset="UTF-8" action={postsPath} className="new_post" id="post-form" method="post">
+        <div className="post-form-select">
+          <label for="post_gender">Gender</label><br/>
+          <div className="post-form-select-background">
+            <select id="post_gender" name="post[gender]">
+              <option value=""></option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option></select>
+            </div>
+        </div>
+
+        <div className="post-form-select">
+          <label for="post_hair">Hair</label><br/>
+          <div className="post-form-select-background"><select id="post_hair" name="post[hair]"><option value=""></option>
+            <option value="Brown">Brown</option>
+            <option value="Black">Black</option>
+            <option value="Blonde">Blonde</option>
+            <option value="Red">Red</option></select></div>
+          </div>
+
+        <label for="post_spotted_at">Location</label>
+        <input id="post_spotted_at" name="post[spotted_at]" type="text"/><br/>
+
+        <label for="post_content">Message</label>
+        <textarea id="post_content" name="post[content]" rows="4"></textarea><br/>
+
+        <input type="submit" value="Submit!"/>
+      </form>
+    )
+  }
+})
 
 var PostList = React.createClass({
   render: function() {
@@ -31,83 +127,23 @@ var PostList = React.createClass({
   }
 });
 
-var PostBox = React.createClass({
-  getInitialState: function() {
-    return {posts: []};
-  },
-  componentDidMount: function() {
-    this.loadPostsFromServer(this.props.batchSize);
-    setInterval(this.loadPostsFromServer, this.props.pollInterval);
-    window.addEventListener('scroll', this.handleScroll);
-  },
-  loadPostsFromServer: function(size) {
-    var size = size || this.state.posts.length
-    
-    $.ajax({
-      url: this.props.url,
-      data: { batch_size: size },
-      dataType: 'json',
-      success: function (data) {
-        this.setState({posts: data.posts});
-      }.bind(this),
-      error: function(xhr, status, err) {
-        console.error(this.props.url, status, err.toString());
-      }.bind(this)
-    });
-  },
-  handleScroll: function() {
-    if ( $(window).scrollTop() >= $(document).height() - $(window).height()) {
-      newSize = this.state.posts.length + this.props.batchSize;
-      this.loadPostsFromServer(newSize);
-    };
-  },
+var Post = React.createClass({
   render: function() {
     return (
-      <div className="postBox" ref="">
-        <PostList posts={this.state.posts} />
+      <div className="post">
+        <p className="location">{this.props.location}: </p>
+        <p className="hair">{this.props.hair}-haired </p>
+        <p className="gender">{this.props.gender}</p>
+        <p className="content">{this.props.content}</p>
+        <a className="post-flag" href="#" title="Flag as inappropriate">X</a>
       </div>
     );
   }
 });
 
-
-
-var reactReady = function() {
+var reactLocationReady = function() {
   React.renderComponent(
-    <PostBox url={postsPath} pollInterval={4000} batchSize={10} />,
-    document.getElementById('location_posts')
+    <LocationDisplay url={postsPath} pollInterval={40000} batchSize={10} />,
+    document.body
   );
-};
-
-var submitPost = function(form) {
-  $.ajax({
-    type: "POST",
-    url: postsPath,
-    data: $(form).serializeArray()
-  })
-  .done(function(response) {
-    newPost = response.post;
-    // Implement displaying new post
-  })
-  .fail(function(response) {
-    var errors = response.responseJSON.errors
-    // Implement error display
-  });
 }
-
-$(".location_posts").ready(function() {
-  reactReady();
-
-  $("form").on("submit", function() {
-    event.preventDefault();
-    submitPost(this);
-  });
-
-  $(window).scroll( function() {
-    if ( $(window).scrollTop() == $(document).height() - $(window).height()) {
-      
-    };
-  });
-
-
-});
--- app/models/location.rb
+++ app/models/location.rb
@@ -5,15 +5,15 @@ class Location < ActiveRecord::Base
 
   after_create { generate_slug }
 
+  def to_param
+    slug
+  end
+
     private
 
   def generate_slug
     self.slug ||= name.downcase.strip.gsub(/ /, '-')
     self.save
-  end
-
-  def to_param
-    slug
   end
 
 end
