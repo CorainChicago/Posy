0bd43cc65119fea1c87302ac204c3727328dd79d
Wil Chandler <wilchandler2@gmail.com>
2014-11-21 14:39:41 -0600

    Fixed flash notice mistake

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -5,7 +5,7 @@ class AdminsController < ApplicationController
     if @admin
       @locations = @admin.locations
     else
-      notice[:alert] = "You must be logged in."
+      flash[:alert] = "You must be logged in."
       redirect_to login_path
     end
   end
