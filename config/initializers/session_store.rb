b5a2a6b2a274944aa860ff9a16a84067edb3ce1a
Wil Chandler <wilchandler2@gmail.com>
2014-12-02 14:56:50 -0600

    Renamed app; added procfile

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_afar_session'
+Rails.application.config.session_store :cookie_store, key: '_posy_session'
