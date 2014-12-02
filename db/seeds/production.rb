3e329806c5ff9a7ff4425e7dffe9e74931a72129
Wil Chandler <wilchandler2@gmail.com>
2014-12-02 15:30:30 -0600

    Changed seed vars to strings

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,3 +1,3 @@
 hendrix = Location.create!(name: "Hendrix College", slug: "hendrix")
-admin = Admin.create(email: ENV[:ORIGINAL_ADMIN_EMAIL], password: ENV[:ORIGINAL_ADMIN_PASSWORD])
+admin = Admin.create(email: ENV["ORIGINAL_ADMIN_EMAIL"], password: ENV["ORIGINAL_ADMIN_PASSWORD"])
 admin.locations << hendrix
