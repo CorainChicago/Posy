b1cdea65069c2596b320b2004939d3c7035fe6c1
Wil Chandler <wilchandler2@gmail.com>
2014-12-07 21:29:13 -0600

    Added demo location to seeds

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,3 +1,7 @@
 hendrix = Location.create!(name: "Hendrix College", slug: "hendrix")
+demo = Location.create!(name: "University of Demonstrations", slug: "demo")
+
 admin = Admin.create(email: ENV["ORIGINAL_ADMIN_EMAIL"], password: ENV["ORIGINAL_ADMIN_PASSWORD"])
 admin.locations << hendrix
+admin.locations << demo
+
