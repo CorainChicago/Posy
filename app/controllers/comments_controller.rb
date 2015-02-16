34f2b77b9c016b1a2942a46a0216f4c2ccb00338
Wil Chandler <wilchandler2@gmail.com>
2015-02-16 12:30:39 -0600

    Cleaned up CommentsController a bit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,20 +8,13 @@ class CommentsController < ApplicationController
     Comment.create(post: post, session_id: session_id, content: content)
 
     respond_to do |format|
-      format.json do 
-        slug = params[:location_slug]
-        size = params[:batchSize]
-
-        redirect_to location_posts_path(location_slug: slug, batch_size: size)
-      end
+      format.json { redirect_to_locations_posts }
     end
   end
 
   def destroy
     # For now, content is not destroyed, only hidden
-    # @comment.destroy
     @comment.mark_as_removed
-
     redirect_to location_admin_path(params[:location_slug])
   end
 
@@ -34,11 +27,19 @@ class CommentsController < ApplicationController
     redirect_to location_admin_path(params[:location_slug])
   end
 
+
+
     private
 
   def get_comment
     id = params[:comment_id] || params[:id]
     @comment = Comment.find_by(id: id)
+  end
+
+  def redirect_to_locations_posts
+    slug = params[:location_slug]
+    size = params[:batchSize]
+    redirect_to location_posts_path(location_slug: slug, batch_size: size)
   end
 
 end
