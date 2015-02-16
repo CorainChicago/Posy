1fa23ef560107ae4ca6ee25d2de3d74381dbea7e
Wil Chandler <wilchandler2@gmail.com>
2015-02-16 12:09:33 -0600

    Removed commented code from LocationsController

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,12 +2,6 @@ class LocationsController < ApplicationController
   before_action :get_location
 
   def show
-
-    # raise params.inspect
-    # url = params[:slug].downcase.strip
-    # @location = Location.find_by(slug: url)
-    # # @post = Post.new
-    # redirect_to '/' unless @location
   end
 
     private
