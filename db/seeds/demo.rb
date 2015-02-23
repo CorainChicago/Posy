cf18c3c5eec4deb10230291e69069011ec7225e7
Wil Chandler <wilchandler2@gmail.com>
2015-02-23 10:42:40 -0600

    Updated demo seeds to delete old data

--- db/seeds/demo.rb
+++ db/seeds/demo.rb
@@ -1,6 +1,21 @@
 demo_location = Location.find_by(slug:"demo") || Location.create(
   slug: "demo", name: 'Demo Location', description: "Demo City, UT")
 
+posts = demo_location.posts
+posts.each do |p|
+  comments = p.comments
+  comments.each do |c|
+    c_flags = c.flaggings
+    c_flags.each { |f| f.destroy }
+    c.destroy
+  end
+
+  p_flags = p.flaggings
+  p_flags.each { |f| f.destroy }
+  p.destroy
+end
+
+
 rand(33..38).times do |n|
   post = Post.new
   post.location = demo_location
