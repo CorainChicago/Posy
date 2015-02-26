88565ca0014bbfab58b651215c151136b2417dc1
Wil Chandler <wilchandler2@gmail.com>
2015-02-26 15:42:56 -0600

    Updated models/comment specs

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,77 +2,51 @@ require 'rails_helper'
 
 RSpec.describe Comment, :type => :model do
   it { should belong_to :post }
-  it { should validate_presence_of :content }
-  it { should validate_presence_of :post_id }
   it { should have_db_index :post_id }
   it { should have_many :flaggings }
+  it { should validate_presence_of :content }
+  it { should validate_presence_of :post_id }
 
-  before(:each) do
-    new_post = Post.create!(
-      location: Location.create(name: "Testing University"),
-      content: "hello there",
-      session_id: "abc",
-      gender: "Male",
-      hair: "Brown",
-      spotted_at: "wherever"
-    )
-    Comment.create(
-      session_id: "0",
-      content: "hey-o",
-      post: new_post
-    )
-  end
-
-  describe '#update_flagged_count' do
-
-    let(:comment) { Comment.find_by(content: "hey-o")}
-
-    it 'should update the flagged column of the record' do
-      # This, perhaps unfortunately, is testing via a Flagging callback that calls this method
-      expect{ Flagging.create(session_id: "0", flaggable: comment) }.to \
-        change{ Comment.find_by(content: "hey-o").flagged }.by(1)
-    end
-
+  it 'implements the content module' do
+    comment = build(:comment)
+    expect(comment).to respond_to(:clear?)
+    expect(comment).to respond_to(:mark_as_cleared)
+    expect(comment).to respond_to(:mark_as_removed)
+    expect(comment).to respond_to(:update_flagged_count)
+    expect(comment).to respond_to(:admin_priority?)
   end
 
   describe '#generate_author_name' do
-    # this is a callback method and will not be called directly here
+    it 'is called upon creation of comment' do
+      comment = build(:comment)
 
-    let(:post) { Post.find_by(content: "hello there") }
-
-    it 'should be called when a comment is first created' do
-      first_comment = Comment.new(post: post, session_id: "0", content: "test")
-
-      expect(first_comment).to receive(:generate_author_name)
-      first_comment.save
+      expect(comment).to receive(:generate_author_name).once
+      comment.save
     end
 
-    it 'should create unique names for unique session_ids' do
-      num_comments = 30
-      authors = []
+    it 'assigns the name "author" if created in same session as associated-post' do
+      session = "test"
+      post = create(:post, session_id: session)
+      comment = create(:comment, post: post, session_id: session)
 
-      num_comments.times do |i|
-        c = Comment.create(post: post, session_id: i.to_s, content: "test")
-        authors << c.author_name
-      end
-
-      expect(authors.compact.uniq.count).to eq num_comments
+      expect(comment.author_name.capitalize).to eq "Author"
     end
 
-    it 'should assign the same name for each comment from a single session' do
-      authors = []
+    it 'assigns the same name if a previous comment has been made by same session' do
+      session = "test"
+      post = create(:post, session_id: session)
+      comment_one = create(:comment, post: post, session_id: session)
+      comment_two = create(:comment, post: post, session_id: session)
 
-      3.times do
-        c = Comment.create(post: post, session_id: "0", content: "test")
-        authors << c.author_name
-      end
-
-      expect(authors.compact.uniq.count).to eq 1
+      expect(comment_one.author_name).to eq comment_two.author_name
     end
 
-    it 'should assign the name "author" to comment from same session as post' do
-      comment = Comment.create(post: post, session_id: post.session_id, content: "test")
-      expect(comment.author_name).to eq "Author"
+    it 'creates unique names for each comment by different session' do
+      post = create(:post_with_comments, comments_count: 35)
+      names = post.comments.pluck(:author_name)
+      uniq  = names.uniq
+
+      expect(names.count).to eq (uniq.count)
     end
 
   end
